f2e2d26afe33c0823d45e045a1f757726a69b270
luminous44 <aklemasultanakhi@gmail.com>
2025-07-12 00:55:17 +0600

    added put,delete and get by id

--- src/main/java/com/example/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/example/journalApp/controller/JournalEntryController.java
@@ -2,6 +2,7 @@ package com.example.journalApp.controller;
 
 import com.example.journalApp.Entity.JournalEntry;
 import com.example.journalApp.services.JournalEntryService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,22 +24,32 @@ private JournalEntryService journalService;
         return journalService.getAll();
     }
     @PostMapping
-    public boolean create(@RequestBody JournalEntry contentEntry){
+    public JournalEntry create(@RequestBody JournalEntry contentEntry){
         contentEntry.setDate(LocalDateTime.now());
         journalService.saveEntry(contentEntry);
-        return true;
+        return contentEntry;
 
     }
 
     @GetMapping("id/{myId}")
-    public JournalEntry getUserById(@PathVariable Long myId){
-        return null;
+    public JournalEntry getUserById(@PathVariable ObjectId myId){
+        return journalService.getById(myId).orElse(null);
     }
 
+   @DeleteMapping("id/{myId}")
+   public boolean deleteUserById(@PathVariable ObjectId myId){
+        journalService.deleteById(myId);
+        return true;
+   }
     @PutMapping("/uid/{userId}")
-    public JournalEntry updateJournalById(@PathVariable Long userId,@RequestBody JournalEntry entry){
-
-        return null;
+    public JournalEntry updateJournalById(@PathVariable ObjectId id,@RequestBody JournalEntry newEntry){
+      JournalEntry old = journalService.getById(id).orElse(null);
+      if (old != null){
+          old.setTitle(newEntry.getTitle() != null && newEntry.getTitle().equals("") ? newEntry.getTitle(): old.getTitle());
+          old.setContent(newEntry.getContent() != null && newEntry.getContent().equals("")? newEntry.getContent(): old.getContent());
+      }
+      journalService.saveEntry(old);
+      return old;
     }
 
 }
--- src/main/java/com/example/journalApp/services/JournalEntryService.java
+++ src/main/java/com/example/journalApp/services/JournalEntryService.java
@@ -2,10 +2,12 @@ package com.example.journalApp.services;
 
 import com.example.journalApp.Entity.JournalEntry;
 import com.example.journalApp.Repository.JournalEntryRepo;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class JournalEntryService {
@@ -20,5 +22,12 @@ public class JournalEntryService {
 
     public List<JournalEntry> getAll(){
         return journalRepo.findAll();
+    }
+    public Optional<JournalEntry> getById(ObjectId id){
+        return journalRepo.findById(String.valueOf(id));
+    }
+
+    public void deleteById(ObjectId id){
+        journalRepo.deleteById(String.valueOf(id));
     }
 }
