9dc0968e5709896be98ef24e4025d5805572a9e2
luminous44 <aklemasultanakhi@gmail.com>
2025-07-14 10:43:05 +0600

    fixed user controller issue beans creation

--- src/main/java/com/example/journalApp/controller/UserDetailsController.java
+++ src/main/java/com/example/journalApp/controller/UserDetailsController.java
@@ -3,32 +3,31 @@ package com.example.journalApp.controller;
 import com.example.journalApp.Entity.User;
 import com.example.journalApp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
-@RequestMapping("/user")
-public class userController {
+@RequestMapping("/users")
+public class UserDetailsController {
     @Autowired
     private UserService userService;
+
     @GetMapping
     public List<User> getAllUse(){
         return userService.getAll();
     }
-    @PutMapping
+    @PostMapping
     public boolean createUser(@RequestBody User user){
         userService.saveUser(user);
         return true;
     }
-    @PutMapping
-    public ResponseEntity<?> updateUser(@RequestBody User user){
+    @PutMapping("/{uerName}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable String uerName){
 
-        User userDb = userService.findByUserName(user.getUserName());
+        User userDb = userService.findByUserName(uerName);
         if (userDb.getUserName() != null){
             userDb.setUserName(user.getUserName());
             userDb.setPassword(user.getPassword());
@@ -38,6 +37,5 @@ public class userController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
     }
-
 
 }
